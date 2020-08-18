4f1da9e92a5be44f286fce326bd941b74c4c5baf
Franklyn Rodriguez <franklynr92@gmail.com>
2020-08-18 15:28:32 -0400

    first starting app create-react-app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import BlogPost from './BlogPost';
 
-function App() {
+
+
+class App extends Component {
+  render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -17,10 +21,14 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+      {/* <BlogPost /> */}
+        
       </header>
     </div>
+
   );
+}
 }
 
 export default App;
